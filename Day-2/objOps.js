a20d12183288e840e3741e42052400f85b128716
SMTarikulIslam <smtarikulislamofficial@gmail.com>
2026-08-30 11:06:57 +0600

    Updated Object example:

--- Day-2/objOps.js
+++ Day-2/objOps.js
@@ -19,7 +19,7 @@ console.log(totalMarks, totalSubject);
 
 
 let avg = (totalMarks / totalSubject).toFixed(2);
-console.log(avg.toFixed(2));
+
 
 if (avg>=80){
     console.log("Passed with distinction. Got  ", avg, "%");
@@ -27,3 +27,24 @@ if (avg>=80){
 else {
     console.log ("Fail korche porashunai focus dite hobe");
 }
+
+// Task: koto gulo iteam kinchen and koto tk bill hoyeche shese BDT te dekhate hobe print kore:
+
+
+
+let cart = [
+    { productName: "Shirt", price: 1200, quantity: 2 },
+    { productName: "Pants", price: 1800, quantity: 1 },
+    { productName: "Socks", price: 150, quantity: 3 },
+];
+
+let totalItems = 0;
+let totalBill = 0;
+
+cart.forEach(item => {
+    totalItems += item.quantity;
+    totalBill += item.price * item.quantity;
+});
+
+console.log("Total Items:", totalItems);
+console.log("Total Bill:", totalBill + " BDT");
